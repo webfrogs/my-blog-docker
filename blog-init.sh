b35c9b9f3d32d980060005d6cffe13aca231f787
Carl Chen <ccf.developer@gmail.com>
2017-12-14 11:29:57 +0800

    let blog ci restart

--- blog-init.sh
+++ blog-init.sh
@@ -2,7 +2,7 @@
 
 docker pull nswebfrog/blog-ci
 
-docker run -d --name blogCI \
+docker run --restart=always -d --name blogCI \
     -p 127.0.0.1:8889:80 \
     -v ~/.ssh:/root/.ssh:ro \
     -v /var/www/blogs:/opt/blog/sites \
