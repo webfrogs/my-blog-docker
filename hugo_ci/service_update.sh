3b33359972e8d79f558f201183b107f86ebffb65
Carl Chen <ccf.developer@gmail.com>
2020-02-20 22:47:23 +0800

    feat: add log file limit for hugo ci

--- hugo_ci/service_update.sh
+++ hugo_ci/service_update.sh
@@ -3,7 +3,9 @@ set -e
 
 docker rm -f hugo_ci || true > /dev/null 2>&1
 docker run --restart=always -d --name hugo_ci \
-    -p 127.0.0.1:8890:80 \
-    -v $HOME/.ssh/hugo_ci:/root/.ssh/id_rsa:ro \
-    -v /var/www/nswebfrog.com:/opt/blog/sites \
-    nswebfrog/hugo_ci
+  -p 127.0.0.1:8890:80 \
+  -v $HOME/.ssh/hugo_ci:/root/.ssh/id_rsa:ro \
+  -v /var/www/nswebfrog.com:/opt/blog/sites \
+  --log-opt max-size=10m \
+  --log-opt max-file=5 \
+  nswebfrog/hugo_ci
